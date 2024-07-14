efe3ebad5ca526476b38397ffd4219440fc6c6e4
Mataura <3121043930@qq.com>
2024-07-14 21:57:42 +0800

    initialize

---
+++ src/main/java/org/aki/resolved/Resolved.java
@@ -0,0 +1,12 @@
+package org.aki.resolved;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Resolved implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
---
+++ src/main/java/org/aki/resolved/client/ResolvedClient.java
@@ -0,0 +1,12 @@
+package org.aki.resolved.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ResolvedClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
