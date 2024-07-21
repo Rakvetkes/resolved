684b4fcda23657bfc258f4d9526bf04eff08d307
Mataura <3121043930@qq.com>
2024-07-21 20:49:18 +0800

    purge

--- src/main/java/org/aki/resolved/manager/FluidServerManager.java
+++ src/main/java/org/aki/resolved/manager/FluidServerManager.java
@@ -43,7 +43,7 @@ public class FluidServerManager implements FluidDataAccessor {
     @Override
     public @NotNull FluidBlockContent getFluidContent(BlockPos pos) {
         FluidChunkData chunk = getChunk(new ChunkPos(pos));
-        return chunk.get(pos.getX() & 15, pos.getY() & 15, pos.getZ());
+        return chunk.get(pos.getX() & 15, pos.getY() & 15, pos.getZ() & 15);
     }
 
     public int getColor(BlockPos pos) {
