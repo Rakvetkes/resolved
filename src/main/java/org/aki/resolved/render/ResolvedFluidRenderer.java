518c127b1dc42ae4472a3ef6e258eba35c1bfd33
greenorange <yangjz1125@outlook.com>
2024-08-05 22:19:53 +0800

    purge the render.

--- src/main/java/org/aki/resolved/render/ResolvedFluidRenderer.java
+++ src/main/java/org/aki/resolved/render/ResolvedFluidRenderer.java
@@ -66,8 +66,18 @@ public class ResolvedFluidRenderer extends SimpleFluidRenderHandler {
      * @param direction From where you can see
      */
     void drawSquare(VertexConsumer vertexConsumer, int light, float x, float y, float z, float d1, float d2, int argb, @NotNull Direction direction, int textureId) {
-        float ul = sprites[textureId].getFrameU(0), ur = sprites[textureId].getFrameU(d1);
-        float vl = sprites[textureId].getFrameV(0), vr = sprites[textureId].getFrameV(d2);
+        float ul, ur, vl, vr;
+        if (textureId == -1) {
+            ul = sprites[0].getFrameU(0);
+            vl = sprites[0].getFrameV(0);
+            ur = sprites[0].getFrameU(1f/16);
+            vr = sprites[0].getFrameV(1f/16);
+        } else {
+            ul = sprites[textureId].getFrameU(0);
+            vl = sprites[textureId].getFrameV(0);
+            ur = sprites[textureId].getFrameU(d1);
+            vr = sprites[textureId].getFrameV(d2);
+        }
         switch (direction) {
             case UP -> {
                 vertexConsumer.vertex(x, y, z).color(argb).texture(ul, vl).light(light).normal(0, 1f, 0);
@@ -151,15 +161,24 @@ public class ResolvedFluidRenderer extends SimpleFluidRenderHandler {
                 last += i.rightFloat();
             }
         }
+        float lasth = layers[0].rightFloat();
+        for (int i = 1; i < layers.length; ++i) {
+            drawSquare(vertexConsumer, getLight(world, pos), fx, y + lasth, fy, 0.998f, 0.998f, layers[i - 1].leftInt(), Direction.UP);
+            drawSquare(vertexConsumer, getLight(world, pos), fx, y + lasth, fy, 0.998f, 0.998f, layers[i].leftInt(), Direction.DOWN);
+            lasth += layers[i].rightFloat();
+        }
         float height = 0f;
         for (IntFloatImmutablePair i : layers)
             height += i.rightFloat();
-        if (shouldRenderBottom(world, pos))
+        // todo Here should adjust the color they show.
+        if (shouldRenderBottom(world, pos)) {
             drawSquare(vertexConsumer, getLight(world, pos), fx, fy, fz,0.998f, 0.998f, layers[0].leftInt(), Direction.DOWN);
-//        if (height >= 1f && shouldRenderBottom(world, pos.up()))
-//            drawSquare(vertexConsumer, getLight(world, pos), fx, fy + 0.998f, fz,0.998f, 0.998f, layers[layers.length - 1].leftInt(), Direction.UP);
-        if (shouldRenderTop(world, pos))
+            drawSquare(vertexConsumer, getLight(world, pos), fx, fy, fz,0.998f, 0.998f, layers[0].leftInt(), Direction.UP);
+        }
+        if (shouldRenderTop(world, pos)) {
             drawSquare(vertexConsumer, getLight(world, pos), fx, fy + height * 0.998f, fz, 0.998f, 0.998f, layers[layers.length - 1].leftInt(), Direction.UP);
+            drawSquare(vertexConsumer, getLight(world, pos), fx, fy + height * 0.998f, fz, 0.998f, 0.998f, layers[layers.length - 1].leftInt(), Direction.DOWN);
+        }
     }
 
 }
